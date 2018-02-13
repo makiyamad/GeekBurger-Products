c8dedc57e60d7de7e3b3fab618effb7abd4c28b3
dmakiyama <dmakiyama@rdisoftware.com>
2018-02-13 12:20:16 -0200

    Step 1 - Adjusting ProductChangedMessage

---
+++ GeekBurger.Products.Contract/ProductChangedMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GeekBurger.Products.Contract
+{
+    public class ProductChangedMessage
+    {
+        public ProductState State { get; set; }
+        public ProductToGet Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+}
