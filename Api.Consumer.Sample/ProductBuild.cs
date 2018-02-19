672a2ff5eb3ede05e6ee679cd15924b86d95675d
dmakiyama <dmakiyama@rdisoftware.com>
2018-02-18 23:30:45 -0300

    Step 6 - Adding Api.Consumer.Sample

---
+++ Api.Consumer.Sample/ProductBuild.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using GeekBurger.Products.Contract;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Api.Consumer.Sample
+{
+    public static class ProductBuild
+    {
+        public static int i = 0;
+        public static byte[] NewProduct() {
+            var product = new ProductToUpsert()
+            {
+                StoreName = i++%2 == 0 ? "Los Angeles - Pasadena" : "Los Angeles - Beverly Hills",
+                Image = "hamb1.png",
+                Name = "Beef the Elegant",
+                Items = new List<ItemToUpsert> {
+                    new ItemToUpsert { Name = "bread" },
+                    new ItemToUpsert { Name = "mustard" },
+                    new ItemToUpsert { Name = "beef" }
+                }
+            };
+
+            var productJson = JsonConvert.SerializeObject(product);
+            return Encoding.UTF8.GetBytes(productJson);
+        }
+    }
+}
---
+++ Api.Consumer.Sample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Api.Consumer.Sample
+{
+    static class Program
+    {
+        private static IConfiguration _configuration;
+        private static string Uri;
+        private static string Subscription;
+
+        static void Main()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Subscription = _configuration.GetSection("product")["subscription"];
+            Uri = _configuration.GetSection("product")["uri"];
+
+            while (true) { 
+                MakeRequest();
+                var exit = Console.ReadLine();
+                if (exit == "exit") break;
+            }
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key",
+                Subscription);
+
+            HttpResponseMessage response;
+
+            using (var content = new ByteArrayContent(ProductBuild.NewProduct()))
+            {
+                content.Headers.ContentType = 
+                    new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(Uri, content);
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseText = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("New Product Added");
+                Console.WriteLine(responseText);
+            }
+            else
+            {
+                Console.WriteLine($"error: {response.StatusCode}");
+            }
+        }
+    }
+}
